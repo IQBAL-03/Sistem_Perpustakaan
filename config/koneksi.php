85dbe1123b4031e8ed56cb8295923127bf27f7aa
IQ-2 <muhamadiqbal9871@gmail.com>
2025-12-20 23:38:42 +0700

    Upload

---
+++ config/koneksi.php
@@ -0,0 +1,14 @@
+<?php
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "db_sisperpus";
+
+$koneksi = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+if(!$koneksi){
+    die("Koneksi Gagal: " . mysqli_connect_error());
+}
+
+mysqli_set_charset($koneksi, "utf8");
+?>
