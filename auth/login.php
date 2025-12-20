73fb07cda53e6a3c8f937c4780cee655a4fe2add
IQ-2 <muhamadiqbal9871@gmail.com>
2025-12-21 01:01:59 +0700

    Upload

--- auth/login.php
+++ auth/login.php
@@ -1,28 +1,28 @@
 <?php
 session_start();
-if(isset($_SESSION['id'])){
+if (isset($_SESSION['id'])) {
     header("Location: ../barang/index.php");
     exit();
 }
 require_once '../config/koneksi.php';
 $error = "";
-if($_SERVER["REQUEST_METHOD"]=="POST"){
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = trim($_POST['username']);
     $password = $_POST['password'];
 
-    if (empty($username) || empty($password)){
+    if (empty($username) || empty($password)) {
         $error = "Username dan Password tidak boleh kosong";
-    }else{
+    } else {
         $sql = "SELECT * FROM pengguna WHERE username = ?";
         $stmt = mysqli_prepare($koneksi, $sql);
         mysqli_stmt_bind_param($stmt, "s", $username);
         mysqli_stmt_execute($stmt);
         $result = mysqli_stmt_get_result($stmt);
 
-        if (mysqli_num_rows($result)==1){
+        if (mysqli_num_rows($result) == 1) {
             $user = mysqli_fetch_assoc($result);
 
-            if (password_verify($password,$user['password'])){
+            if (password_verify($password, $user['password'])) {
                 $_SESSION['id'] = $user['id'];
                 $_SESSION['nama'] = $user['nama'];
                 $_SESSION['username'] = $user['username'];
@@ -30,10 +30,10 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
 
                 header("Location: ../barang/index.php?status=login_sukses");
                 exit();
-            }else{
+            } else {
                 $error = "Password salah !";
             }
-        }else{
+        } else {
             $error = "Username Tidak Ditemukan";
         }
 
@@ -41,4 +41,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
     }
 }
 $judul = "Login";
+?>
+<?php
+mysqli_close($koneksi);
 ?>
