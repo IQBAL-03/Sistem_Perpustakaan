eae7642a86c612ed23adc29081141d438df735f7
IQ-2 <muhamadiqbal9871@gmail.com>
2026-01-14 14:40:43 +0700

    feat: memperbaiki bug

--- partials/header.php
+++ partials/header.php
@@ -28,11 +28,15 @@ if (session_status() == PHP_SESSION_NONE) {
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow-sm">
         <div class="container">
-            <a class="navbar-brand fw-bold" href="/sistem-perpus/index.php">
+            <?php
+            if (isset($_SESSION['user_id'])){
+                $link = 'href = "/sistem-perpus/dashboard/index.php"';
+            }else{
+                $link ='';
+            }
+            ?>
+            <a class="navbar-brand fw-bold" <?=$link?>>
                 <i class="bi bi-book-half"></i> Sistem Perpustakaan</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMenu">
-                <span class="navbar-toggler-icon"></span>
-            </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <?php if (isset($_SESSION['user_id']) && $_SESSION['role'] == 'admin'): ?>
